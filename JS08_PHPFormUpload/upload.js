0682ab872c0f4f407f859163c482a4a4683e0e60
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-16 16:20:35 +0700

    JS08_Practicum 4_Decorate Upload Files

--- JS08_PHPFormUpload/upload.js
+++ JS08_PHPFormUpload/upload.js
@@ -1,8 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // Ketika file dipilih, ubah status tombol upload
+    $('#file').change(function () {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
+
+    // Ketika form di-submit
     $('#upload-form').submit(function (e) {
-        e.preventDefault();
+        e.preventDefault(); // Mencegah pengiriman form secara default
         var formData = new FormData(this);
-        
+
         $.ajax({
             type: 'POST',
             url: 'upload_ajax.php',
@@ -13,8 +23,8 @@ $(document).ready(function() {
             success: function (response) {
                 $('#status').html(response);
             },
-            error: function(){
-                $('#status').html('An error occurred while uploading the files');
+            error: function () {
+                $('#status').html('Something wrong when upload file.');
             }
         });
     });
