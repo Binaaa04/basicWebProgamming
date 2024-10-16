e1233c62720861648393d87778b10737ad3cd308
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-16 08:44:16 +0700

    JS08_Practicum 3_Modify to upload photos

--- JS08_PHPFormUpload/upload_ajax.php
+++ JS08_PHPFormUpload/upload_ajax.php
@@ -1,26 +1,35 @@
 <?php
-if (isset($_FILES['file'])) {
-         $errors = array();
-         $file_name = $_FILES['file']['name'];
-         $file_size = $_FILES['file']['size'];
-         $file_tmp = $_FILES['file']['tmp_name'];
-         $file_type = $_FILES['file']['type'];
-         @$file_ext = strtolower(end(explode('.', $_FILES['file']['name'])));
-         $extensions = array("pdf", "doc", "docx", "txt");
+if (isset($_FILES['files'])) {
+    $errors = array();
+    $extensions = array("jpg", "jpeg", "png", "gif");
     
-         if (in_array($file_ext, $extensions) === false) {
-             $errors[] = "Allowed file extensions are PDF, DOC, DOCX, or TXT";
-         }
-    
-         if ($file_size > 2097152) {
-             $errors[] = 'File size should not exceed 2 MB';
-         }
-    
-         if (empty($errors) == true) {
-             move_uploaded_file($file_tmp, "documents/" . $file_name);
-             echo "file uploaded successfully";
-         } else {
-             echo implode(" ", $errors);
-         }
+    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['files']['name'][$key];
+        $file_size = $_FILES['files']['size'][$key];
+        $file_tmp = $_FILES['files']['tmp_name'][$key];
+        $file_type = $_FILES['files']['type'][$key];
+        @$file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$key])));
+        
+        if (in_array($file_ext, $extensions) === false) {
+            $errors[] = "$file_name: Allowed file types are JPG, JPEG, PNG, and GIF.";
         }
+        
+        if ($file_size > 2097152) {
+            $errors[] = "$file_name: File size should not exceed 2 MB.";
+        }
+        
+        if (empty($errors) == true) {
+            if (!file_exists('images')) {
+                mkdir('images', 0777, true);
+            }
+            move_uploaded_file($file_tmp, "images/" . $file_name);
+        }
+    }
+
+    if (empty($errors)) {
+        echo "Files uploaded successfully";
+    } else {
+        echo implode(" ", $errors);
+    }
+}
 ?>
