f476d87544aabeb137645284a522a54b3c3a317d
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-16 08:41:18 +0700

    JS08_Practicum 3_Modify to Upload Images

--- JS08_PHPFormUpload/form_upload_ajax.php
+++ JS08_PHPFormUpload/form_upload_ajax.php
@@ -1,15 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Upload File Document</title>
-    </head>
-    <body>
-        <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file" id="file">
-        <input type="submit" name="submit" value="submit">
-        </form>
-        <div id="status"></div>
-        <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
-        <script src="upload.js"></script>
-    </body>
-</html>
+<?php
+if (isset($_FILES['files'])) {
+    $errors = array();
+    $extensions = array("jpg", "jpeg", "png", "gif");
+    
+    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+        $file_name = $_FILES['files']['name'][$key];
+        $file_size = $_FILES['files']['size'][$key];
+        $file_tmp = $_FILES['files']['tmp_name'][$key];
+        $file_type = $_FILES['files']['type'][$key];
+        @$file_ext = strtolower(end(explode('.', $_FILES['files']['name'][$key])));
+        
+        if (in_array($file_ext, $extensions) === false) {
+            $errors[] = "$file_name: Allowed file types are JPG, JPEG, PNG, and GIF.";
+        }
+        
+        if ($file_size > 2097152) {
+            $errors[] = "$file_name: File size should not exceed 2 MB.";
+        }
+        
+        if (empty($errors) == true) {
+            if (!file_exists('images')) {
+                mkdir('images', 0777, true);
+            }
+            move_uploaded_file($file_tmp, "images/" . $file_name);
+        }
+    }
+
+    if (empty($errors)) {
+        echo "Files uploaded successfully";
+    } else {
+        echo implode(" ", $errors);
+    }
+}
+?>
