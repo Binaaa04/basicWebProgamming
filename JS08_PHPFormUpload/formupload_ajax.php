4ae8a7f7b4781b5b3b9e96e54a311af50a61c8fd
Binaa <143788742+Binaaa04@users.noreply.github.com>
2024-10-16 16:19:47 +0700

    JS08_Practicum 4_Decorate Upload Files

--- JS08_PHPFormUpload/formupload_ajax.php
+++ JS08_PHPFormUpload/formupload_ajax.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link rel="stylesheet" type="text/css" href="upload.css">
         <title>Upload File Document</title>
     </head>
     <body>
+    <body>
+    <div class="upload-form-container">
+        <h2>Unggah File Dokumen</h2>
         <form id="upload-form" action="upload_ajax.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file" id="file">
-        <input type="submit" name="submit" value="submit">
+        <div class="file-input-container">
+                <input type="file" name="file" id="file" class="file-input">
+                <label for="file">Select File</label>
+            </div>
+            <button type="submit" name="submit" class="upload-button" id="upload-button" disabled>Upload</button>
         </form>
-        <div id="status"></div>
+        <div id="upload-status" class="upload-status"></div>
+    </div>
         <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
         <script src="upload.js"></script>
     </body>
